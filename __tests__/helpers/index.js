5f9d9dd73dbed4b78276aef8f576477294198bd1
Alla Magina <babara@flylady.su>
2026-03-20 15:39:53 +0300

    feat: add user and test data fixtures, implement session and user CRUD tests

---
+++ __tests__/helpers/index.js
@@ -0,0 +1,23 @@
+import { URL } from 'url'
+import fs from 'fs'
+import path from 'path'
+
+const getFixturePath = filename => path.join('..', '..', '__fixtures__', filename)
+const readFixture = filename => fs.readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8').trim()
+const getFixtureData = filename => JSON.parse(readFixture(filename))
+
+export const getTestData = () => getFixtureData('testData.json')
+
+export const cleanDb = async (knex) => {
+  const tables = ['tasks_labels', 'tasks', 'labels', 'task_statuses', 'users']
+  for (const table of tables) {
+    await knex(table).truncate()
+  }
+}
+
+export const prepareData = async (app) => {
+  const { knex } = app.objection
+
+  // получаем данные из фикстур и заполняем БД
+  await knex('users').insert(getFixtureData('users.json'))
+}
